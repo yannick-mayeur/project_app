c176da572bd35c12587d6be364e341e8898252a1
Yannick Mayeur <yannick.mayeur@gmail.com>
2018-05-15 20:18:59 +0200

    Add Course entity through scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :courses
   resources :exams
   root 'application#hello'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_15_181525) do
+ActiveRecord::Schema.define(version: 2018_05_15_181836) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "exams", force: :cascade do |t|
     t.integer "course_id"
