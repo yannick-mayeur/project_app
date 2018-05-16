f56c746ff802a16677d0b36663ce84d7e9c03b31
Yannick Mayeur <yannick.mayeur@gmail.com>
2018-05-16 16:03:16 +0200

    Add uniqueness validation of email
    Email will be used as login so it needs to be unique.
    
    To ensure that emails are checked for uniqueness in the
    application as well as in the databse where they are stored once they
    are downcased.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+  before_save { self.email = self.email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[a-z]*\.([a-z]*-?[a-z])*[1-9]?@(etu.)?umontpellier.fr\z/i
   validates :email, presence: true, length: { maximum: 255 },
-    format: { with: VALID_EMAIL_REGEX }
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false }
 end
