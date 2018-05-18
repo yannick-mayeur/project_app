a3b7cdd0c48ef88c53007cd39487f31df760767c
Yannick Mayeur <yannick.mayeur@gmail.com>
2018-05-19 01:03:21 +0200

    Add integration test for signup form
     * test if urls are right
     * test show of error messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   get '/signup', to: 'users#new'
   get '/about',  to: 'static_pages#about'
-  post '/users', to: 'users#create'
+  post '/signup', to: 'users#create'
   root 'static_pages#home'
 end
