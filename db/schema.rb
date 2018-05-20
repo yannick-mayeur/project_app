add42b7a5cc78457b59f4076a417952edffd5fce
Yannick Mayeur <yannick.mayeur@gmail.com>
2018-05-20 23:21:32 +0200

    Add Course model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_19_141915) do
+ActiveRecord::Schema.define(version: 2018_05_20_212110) do
+
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
