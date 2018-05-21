ad1a2830925cf14ce0784e80706273a8accdc653
Yannick Mayeur <yannick.mayeur@gmail.com>
2018-05-21 23:32:29 +0200

    Update integration test for layout
    Added test for link for courses and login/logout/edit user.

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -7,6 +7,16 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", courses_path
+    assert_select "a[href=?]", login_path
+  end
+
+  test "layout links logged in" do
+    @user = users(:foobar)
+    log_in_as(@user)
+    get root_path
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", edit_user_path(@user)
   end
 
   test "home links" do
