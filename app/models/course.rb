54d44074cf6493c5ee348b1001753ed4239492ef
Yannick Mayeur <yannick.mayeur@gmail.com>
2018-05-22 12:50:25 +0200

    Removed followment model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,4 @@
 class Course < ApplicationRecord
-  has_many :followments
-  has_many :users, through: :followments
   validates :name, presence: true, length: { maximum: 50 }
   validates :description, presence: true, length: { maximum: 255 }
 end
--- app/models/followment.rb
+++
@@ -1,4 +0,0 @@
-class Followment < ApplicationRecord
-  belongs_to :course
-  belongs_to :user
-end
