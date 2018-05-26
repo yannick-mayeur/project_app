177e8b7a68e795d80f1c88d7b44efd975e66c90c
Yannick Mayeur <yannick.mayeur@gmail.com>
2018-05-26 20:24:47 +0200

    Add ajax for (un)follow button

--- app/controllers/followments_controller.rb
+++ app/controllers/followments_controller.rb
@@ -2,18 +2,25 @@ class FollowmentsController < ApplicationController
   before_action :logged_in_user
 
   def create
-    course = Course.find(params[:course_id])
-    if current_user.courses.exists?(course.id)
-    else
-      current_user.courses<<course
+    @course = Course.find(params[:course_id])
+    if current_user.courses.exists?(@course.id)
       redirect_to courses_path
+    else
+      current_user.courses<<@course
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
     end
   end
 
   def destroy
-    course = Followment.find(params[:id]).course
-    current_user.courses.delete(course)
-    redirect_to courses_path
+    @course = Followment.find(params[:id]).course
+    current_user.courses.delete(@course)
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.js
+    end
   end
 
 end
