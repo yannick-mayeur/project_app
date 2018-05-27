482221f3d41ce3abf410a64be3552904d6f97f7e
Yannick Mayeur <yannick.mayeur@gmail.com>
2018-05-27 11:57:17 +0200

    Add edit course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -20,6 +20,20 @@ class CoursesController < ApplicationController
     end
   end
 
+  def edit
+    @course = Course.find(params[:id])
+  end
+
+  def update
+    @course = Course.find(params[:id])
+    if @course.update_attributes(user_params)
+      flash[:success] = "Successful edit!"
+      redirect_to courses_path
+    else
+      render 'edit'
+    end
+  end
+
   def destroy
     Course.find(params[:id]).destroy
     flash[:success] = "Course deleted"
