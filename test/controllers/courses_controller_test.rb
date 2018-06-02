2255d45addcbdb8629cfc7d32916fbd738dc8bdc
Yannick Mayeur <yannick.mayeur@gmail.com>
2018-06-02 21:29:00 +0200

    Add tests for courses controller

--- test/controllers/courses_controller_test.rb
+++ test/controllers/courses_controller_test.rb
@@ -10,6 +10,12 @@ class CoursesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should show course" do
+    course = Course.first
+    get course_path(course)
+    assert_response :success
+  end
+
   test "should get new" do
     log_in_as(@user)
     get new_course_url
@@ -23,4 +29,42 @@ class CoursesControllerTest < ActionDispatch::IntegrationTest
     assert_not flash.empty?
   end
 
+  test "should create course" do
+    log_in_as(@user)
+    assert_difference('Course.count') do
+      post courses_path, params: { course: { name: "foobar", description: "foobar" } }
+    end
+    assert_redirected_to courses_path
+  end
+
+  test "should get edit" do
+    log_in_as(@user)
+    course = Course.first
+    get edit_course_path(course)
+    assert_response :success
+  end
+
+  test "should not get edit because not logged in" do
+    course = Course.first
+    get edit_course_path(course)
+    assert_redirected_to login_path
+  end
+
+  test "should update course" do
+    log_in_as(@user)
+    course = Course.first
+    patch course_path(course), params: { course: { name: "updated" } }
+    assert_redirected_to courses_path
+    course.reload
+    assert_equal "updated", course.name
+  end
+
+  test "should destroy course" do
+    log_in_as(@user)
+    course = Course.first
+    assert_difference('Course.count', -1) do
+      delete course_path(course)
+    end
+    assert_redirected_to courses_path
+  end
 end
