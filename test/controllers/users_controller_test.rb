8ba7927007c8f1357b8fd6c209a4221b0b5685a0
Yannick Mayeur <yannick.mayeur@gmail.com>
2018-06-02 21:08:29 +0200

    Add tests for user controller

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -6,4 +6,24 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should create user" do
+    assert_difference('User.count') do
+      post signup_url, params: { user: { name: "foobar",
+                                         email: "foo.bar@etu.umontpellier.fr",
+                                         password: "password",
+                                         password_confirmation: "password" } }
+    end
+    assert_redirected_to root_path
+  end
+
+  test "should update user" do
+    user = User.first
+    # We have to be logged in to be able to update user
+    log_in_as(user)
+    patch user_path(user), params: { user: { name: "updated" } }
+    assert_redirected_to root_path
+    user.reload
+    assert_equal "updated", user.name
+  end
+
 end
