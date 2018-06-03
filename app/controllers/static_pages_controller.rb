c31530221597e1dbd7083259d87e8de82163eddc
Yannick Mayeur <yannick.mayeur@gmail.com>
2018-06-03 12:22:18 +0200

    Fix home page when not logged in

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
-    @user ||= current_user
-    @courses = @user.courses
-    @courses = @user.courses.paginate(page: params[:page], per_page: 10)
+    if logged_in?
+      @user ||= current_user
+      @courses = @user.courses
+      @courses = @user.courses.paginate(page: params[:page], per_page: 10)
+    end
   end
 
   def about
