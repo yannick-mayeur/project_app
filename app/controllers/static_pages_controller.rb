b73f22c5a44c6f510ef17f5a206ef429620083d1
Yannick Mayeur <yannick.mayeur@gmail.com>
2018-06-03 12:14:16 +0200

    Fix pagination on home page

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
+    @user ||= current_user
+    @courses = @user.courses
+    @courses = @user.courses.paginate(page: params[:page], per_page: 10)
   end
 
   def about
