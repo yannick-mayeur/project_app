7824134d5c2f6ccd2317a6db44a65ad75998ed95
Yannick Mayeur <yannick.mayeur@gmail.com>
2018-09-21 17:47:34 +0200

    Update User model to add activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,14 @@
 class User < ApplicationRecord
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
   has_many :followments
   has_many :courses, through: :followments
   has_many :comments
 
   before_save { self.email = self.email.downcase }
+  before_create :create_activation_digest
+
+
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[a-z]*\.([a-z]*-?[a-z])*[1-9]?@(etu.)?umontpellier.fr\z/i
   validates :email, presence: true, length: { maximum: 255 },
@@ -40,4 +43,13 @@ class User < ApplicationRecord
   def forget
     update_attribute(:remember_digest, nil)
   end
+
+
+  private
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
+  end
+
 end
